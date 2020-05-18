4a8dda48b15b19f829a5360febe708e8f1885f8e
nemundo <urs.lang@gmail.com>
2020-05-18 10:42:20 +0200

    .

--- src/File/Pdf/PdfFile.php
+++ src/File/Pdf/PdfFile.php
@@ -4,19 +4,22 @@
 namespace Nemundo\Core\File\Pdf;
 
 
+use Nemundo\Core\Debug\Debug;
+
 class PdfFile extends AbstractFile
 {
 
     public function getPdfText()
     {
 
-        $command = "pdftotext -eol unix $this->filename -";
+        $command = "pdftotext $this->filename -";
         $text = shell_exec($command);
 
         if ($text === null) {
             $text = '';
         }
-
+        (new Debug())->write($text);
+        exit;
         return $text;
 
     }
