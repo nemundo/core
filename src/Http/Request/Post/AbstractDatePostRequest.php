5f223c79c041febfa4581131610329ac8a0740a0
nemundo <urs.lang@gmail.com>
2020-05-29 20:52:17 +0200

    .

--- src/Http/Request/Post/AbstractDatePostRequest.php
+++ src/Http/Request/Post/AbstractDatePostRequest.php
@@ -11,7 +11,10 @@ abstract class AbstractDatePostRequest extends AbstractPostRequest
 
     public function getDate() {
 
+        $date=null;
+        if ($this->hasValue()) {
         $date = (new Date())->fromGermanFormat($this->getValue());
+        }
         return $date;
 
     }
