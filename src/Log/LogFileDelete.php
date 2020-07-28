646373801fbca0e925a8551a27054f494380b4ba
unknown <urs.lang@gmail.com>
2020-07-28 15:37:24 +0200

    .

--- src/Log/LogFileDelete.php
+++ src/Log/LogFileDelete.php
@@ -4,22 +4,15 @@ namespace Nemundo\Core\Log;
 
 
 use Nemundo\Core\Base\AbstractBase;
-use Nemundo\Core\File\Directory;
 use Nemundo\Project\Path\LogPath;
 
 class LogFileDelete extends AbstractBase
 {
 
-
     public function deleteLog()
     {
 
-        (new LogPath())->deleteDirectory(false);
-
-        /*
-        (new Directory(LogConfig::$logPath))
-            ->deleteDirectory(false);
-*/
+        (new LogPath())->emptyDirectory();
 
     }
 
