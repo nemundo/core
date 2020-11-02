55a939912eb8c734cb318cee4b82dcc95c631aba
unknown <urs.lang@gmail.com>
2020-11-02 23:39:10 +0100

    .

--- test/WebRequest/WebRequest.php
+++ test/WebRequest/WebRequest.php
@@ -3,22 +3,15 @@
 
 require __DIR__.'/../config.php';
 
-$url='https://www.zamg.ac.at/fix/wetter/bodenkarte/2020/08/21/BK_BodAna_Sat_2008210000.png';
+//$url='https://www.zamg.ac.at/fix/wetter/bodenkarte/2020/08/21/BK_BodAna_Sat_2008210000.png';
 //$url = 'https://www.zamg.ac.at/fix/wetter/bodenkarte/2020/08/21/BK_BodAna_Sat_20082asdf10000.png';
+
+
+$url = 'https://th.bing.com/th/id/OIP.PL5VicpCLu3UIQV9JDM6gAHaE6?pid=Api&rs=1';
+
 $filename = (new \Nemundo\Project\Path\TmpPath())->addPath('web_request.png')->getFilename();
-
-//(new \Nemundo\Core\Debug\Debug())->write(get_headers($url));
-
 $responseCode =  (new \Nemundo\Core\WebRequest\WebRequest())->getResponseCode($url);
 (new \Nemundo\Core\Debug\Debug())->write($responseCode);
 
-//get_headers($url)
-
-
-
-
-//$html = (new \Nemundo\Core\WebRequest\WebRequest())->getUrl($url);
-//(new \Nemundo\Core\Debug\Debug())->write($html);
-
-//(new \Nemundo\Core\WebRequest\WebRequest())->downloadUrl($url,$filename);
+(new \Nemundo\Core\WebRequest\WebRequest())->downloadUrl($url,$filename);
 
