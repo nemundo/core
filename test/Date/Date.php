811852e86e9b06663e69518f1d4fb6955f1b2af9
nemundo <urs.lang@gmail.com>
2021-01-27 09:09:42 +0100

    .

--- test/Date/Date.php
+++ test/Date/Date.php
@@ -7,11 +7,14 @@ require __DIR__.'/../config.php';
 
 $date = (new \Nemundo\Core\Type\DateTime\Date('2020-12-31'));
 (new \Nemundo\Core\Debug\Debug())->write($date->getWeekNumber());
-(new \Nemundo\Core\Debug\Debug())->write($date->getYear());
+(new \Nemundo\Core\Debug\Debug())->write($date->getFormat('o'));
 
 $date = (new \Nemundo\Core\Type\DateTime\Date('2021-01-01'));
 (new \Nemundo\Core\Debug\Debug())->write($date->getWeekNumber());
-(new \Nemundo\Core\Debug\Debug())->write($date->getYear());
+(new \Nemundo\Core\Debug\Debug())->write($date->getFormat('o'));
+
+
+
 
 
 
