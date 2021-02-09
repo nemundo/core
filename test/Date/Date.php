36722a46a62307069f7eb296829ecc7006c785fc
nemundo <urs.lang@gmail.com>
2021-02-09 18:23:21 +0100

    .

--- test/Date/Date.php
+++ test/Date/Date.php
@@ -3,8 +3,14 @@
 require __DIR__.'/../config.php';
 
 
-//$date = (new \Nemundo\Core\Type\DateTime\Date())->setNow();
 
+
+
+$date = (new \Nemundo\Core\Type\DateTime\Date())->fromGermanLongFormat('8. Februar 2021');
+(new \Nemundo\Core\Debug\Debug())->write($date->getIsoDateFormat());
+
+
+/*
 $date = (new \Nemundo\Core\Type\DateTime\Date('2020-12-31'));
 (new \Nemundo\Core\Debug\Debug())->write($date->getWeekNumber());
 (new \Nemundo\Core\Debug\Debug())->write($date->getFormat('o'));
@@ -12,7 +18,7 @@ $date = (new \Nemundo\Core\Type\DateTime\Date('2020-12-31'));
 $date = (new \Nemundo\Core\Type\DateTime\Date('2021-01-01'));
 (new \Nemundo\Core\Debug\Debug())->write($date->getWeekNumber());
 (new \Nemundo\Core\Debug\Debug())->write($date->getFormat('o'));
-
+*/
 
 
 
