06026fdf763a38500a3a81e8efe7bc2815394041
unknown <urs.lang@gmail.com>
2021-03-30 13:17:44 +0200

    .

---
+++ test/Config/ConfigFile.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/../config.php';
+
+$filename = __DIR__.'/test.ini';
+
+$file = new \Nemundo\Core\Config\ConfigFile($filename);
+(new \Nemundo\Core\Debug\Debug())->write($file->getValue('test'));
+$file->setValue('mountain','berg2');
+$file->writeFile();
