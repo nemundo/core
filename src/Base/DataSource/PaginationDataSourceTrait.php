ac45e7b0bd196736c2c2a75ea80be5cdfee6800e
Urs Lang <urs.lang@gmail.com>
2022-08-27 23:11:37 +0200

    .

---
+++ src/Base/DataSource/PaginationDataSourceTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nemundo\Core\Base\DataSource;
+
+trait PaginationDataSourceTrait
+{
+
+
+    public $currentPage;
+
+    /**
+     * @var int
+     */
+    public $paginationLimit = 30;
+
+
+}
