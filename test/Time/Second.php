64915518b04fd2ceaaaec89b50fb63f8075402ba
Urs Lang <urs.lang@gmail.com>
2023-04-20 11:30:10 +0200

    .

--- test/Time/Second.php
+++ test/Time/Second.php
@@ -1,1 +1,1 @@
-<?phprequire '../config.php';$second=new \Nemundo\Core\Time\Second(60);(new \Nemundo\Core\Debug\Debug())->write($second->getText());
+<?phprequire __DIR__ . '/../config.php';$second = new \Nemundo\Core\Time\Second(125);(new \Nemundo\Core\Debug\Debug())->write($second->getText());
