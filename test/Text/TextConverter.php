d181cde7b5f5652401cafeeaa0a27cb23de34e99
Urs Lang <urs.lang@luzern.com>
2026-08-19 10:16:41 +0200

    .

--- test/Text/TextConverter.php
+++ test/Text/TextConverter.php
@@ -5,4 +5,8 @@ use Nemundo\Core\Text\TextConverter;
 require __DIR__ . '/../config.php';
 
 $text = '„Wiedergeburt einer Königsberger Kastenkrippe – Ein Beitrag zur Krippenkunst des Egerlandes“';
+$text = 'bundesgesetz-ueber-die-allgemeinverbindlicherklaerung-von-gesamtarbeitsvertraegen
+allgemeinverbindlicherklaerung-von-mindestloehnen-die-unter-kantonalen-mindestloehnen-liegen';
+
+
 (new \Nemundo\Core\Debug\Debug())->write((new TextConverter())->convertToUrl($text));
